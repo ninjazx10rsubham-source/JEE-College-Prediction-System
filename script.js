962583b27631bd42e18481738a7e1fec67dc486f
ninjazx10rsubham-source <ninjazx10rsubham@gmail.com>
2026-05-02 22:07:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,77 @@
+function predict() {
+    <div class="result-box">
+
+      <h2>Prediction Result</h2>
+
+      <p><strong>Marks:</strong> ${marks}</p>
+
+      <p><strong>Estimated Percentile:</strong>
+      ${closest.percentile}</p>
+
+      <p><strong>Estimated Rank:</strong>
+      ${closest.rank}</p>
+
+      <p><strong>Performance:</strong>
+      ${performance}</p>
+
+      <h2 style="margin-top:25px;">
+      Predicted Colleges
+      </h2>
+  `
+
+  const filtered = colleges.filter(college =>
+
+    college.category === category &&
+
+    closest.rank <= college.closingRank * 1.1
+  )
+
+  if (filtered.length === 0) {
+
+    html += `
+      <p style="margin-top:15px;">
+      No colleges found for this range.
+      </p>
+    `
+  }
+
+  filtered.forEach(college => {
+
+    html += `
+
+      <div class="college">
+
+        <h3>${college.institute}</h3>
+
+        <p>${college.branch}</p>
+
+        <p>
+        Closing Rank: ${college.closingRank}
+        </p>
+
+      </div>
+    `
+  })
+
+  html += `</div>`
+
+  document.getElementById('result').innerHTML = html
+}
+
+const themeBtn = document.getElementById('themeBtn')
+
+let darkMode = false
+
+themeBtn.addEventListener('click', () => {
+
+  darkMode = !darkMode
+
+  document.body.classList.toggle('dark-mode')
+
+  if (darkMode) {
+    themeBtn.innerText = '☀️ Light Mode'
+  }
+  else {
+    themeBtn.innerText = '🌙 Dark Mode'
+  }
+})
